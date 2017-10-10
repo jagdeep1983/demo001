7883f26d7f1232162f87efbb07ff016f89daca3e
jagdeep1983 <32670893+jagdeep1983@users.noreply.github.com>
2017-10-10 13:45:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 echo "<h1>Hello World Version 2</h1>";
 echo "<p>Hello World this is Jagdeep Singh</p>";
 echo "<p>This is new peragrph</p>";
-echo "<p><a herf='peopleapi.php'>Link to People API</a></p>";
+echo "<p><a href='peopleapi.php'>Link to People API</a></p>";
 
