48b69752710db7a6d568fe203f98358ed8997c90
jagdeep1983 <32670893+jagdeep1983@users.noreply.github.com>
2017-10-10 13:00:19 +0100

    new message

---
+++ jsonapi.php
@@ -0,0 +1,6 @@
+$items + array();
+for($i = 0; $i < 10; $i++) {
+$item[$i] = "This is message of Jagdeep Singh $i";
+header('Content-Type: application/json');
+$jsonOutput = json_encode($items);
+echo $jsonOutput;
