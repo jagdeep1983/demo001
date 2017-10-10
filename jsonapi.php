9f5124161c7fda66a63d20c8b7d45d66c4ce7ac1
jagdeep1983 <32670893+jagdeep1983@users.noreply.github.com>
2017-10-10 13:13:50 +0100

    Update jsonapi.php

--- jsonapi.php
+++ jsonapi.php
@@ -1,7 +1,7 @@
 <?php
 $items = array();
 for($i = 0; $i < 10; $i++) {
-$item[$i] = "This is message of Jagdeep Singh $i";
+$items[$i] = "This is message of Jagdeep Singh $i";
 }
 header('Content-Type: application/json');
 $jsonOutput = json_encode($items);
